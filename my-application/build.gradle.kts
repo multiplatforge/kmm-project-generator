47b5f991deeec1b02076fed96d101f968a40b258
Alexandre Gressier <alexandre@gressier.dev>
2022-03-25 16:56:38 +0100

    Upgrade AGP from 7.3.0-alpha03 to 7.3.0-alpha07

--- my-application/build.gradle.kts
+++ my-application/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20-M1")
-        classpath("com.android.tools.build:gradle:7.3.0-alpha03")
+        classpath("com.android.tools.build:gradle:7.3.0-alpha07")
     }
 }
 

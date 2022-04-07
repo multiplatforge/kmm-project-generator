c46f8d146bd7505cd18261d0341dd34b7c0399e3
Alexandre Gressier <alexandre@gressier.dev>
2022-04-07 23:21:20 +0200

    Migrate from the `buildscript` block the plugins DSL

--- my-application/build.gradle.kts
+++ my-application/build.gradle.kts
@@ -1,13 +1,6 @@
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20-M1")
-        classpath("com.android.tools.build:gradle:7.3.0-alpha07")
-    }
+plugins {
+    kotlin("android") version "1.6.20-M1" apply false
+    id("com.android.application") version "7.3.0-alpha07" apply false
 }
 
 tasks {

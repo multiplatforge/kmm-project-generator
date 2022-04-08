f8a8c5254c4c50aa7c8490eeb6bc47463faa8231
Alexandre Gressier <alexandre@gressier.dev>
2022-04-08 11:04:24 +0200

    Collapse all the iOS `sourceSets` blocks into one

--- my-application/myapp-shared/build.gradle.kts
+++ my-application/myapp-shared/build.gradle.kts
@@ -8,9 +8,7 @@ version = "1.0"
 
 kotlin {
     android()
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+    ios()
 
     sourceSets {
         val commonMain by getting
@@ -21,24 +19,8 @@ kotlin {
         }
         val androidMain by getting
         val androidTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
+        val iosMain by getting
+        val iosTest by getting
     }
 
     cocoapods {

62e09bf0bea3a716d5287124d949131a7e0cbdcf
Alexandre Gressier <alexandre@gressier.dev>
2022-04-08 10:55:05 +0200

    Delete the Cocoapods summary & homepage for the shared module

--- my-application/myapp-shared/build.gradle.kts
+++ my-application/myapp-shared/build.gradle.kts
@@ -13,8 +13,6 @@ kotlin {
     iosSimulatorArm64()
 
     cocoapods {
-        summary = "Some description for the Shared Module"
-        homepage = "Link to the Shared Module homepage"
         ios.deploymentTarget = "14.1"
         podfile = project.file("../myapp-ios-app/Podfile")
         framework {
